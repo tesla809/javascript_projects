a3a761d39b4fd547201db5c014365818a97f55be
Anthony Albertorio <anthony.albertorio@gmail.com>
2014-12-11 23:08:48 -0500

    first commit of rock_paper_sissors.js game

---
+++ rock_paper_scissors.js
@@ -0,0 +1,63 @@
+/*
+Rock destroys scissors.
+Scissors cut paper.
+Paper covers rock.
+
+Our code will break the game into 3 phases:
+a. User makes a choice
+b. Computer makes a choice
+c. A compare function will determine who wins
+*/
+
+
+// rock, paper scissor game.
+
+var intro = function () {
+    
+console.log("This is the rock, paper, scissors game.");
+console.log("Pick one to play against the computer.");
+
+}
+
+var user_Input = function () { 
+    // pick weapon.
+    console.log("Type your choice: Rock, Paper, Scissor or choose, 1, 2, 3 respectively.");
+    var weapon = prompt("Pick your weapon");
+    return selection_List(weapon);
+};
+
+var selection_List = function (weapon){
+    // ensures user picks within choices
+    if (weapon === "rock" || weapon === "Rock" || weapon === "1") {
+        console.log("Rock!");
+        return weapon;
+        
+    } else if (weapon === "paper" || weapon === "Paper" || weapon === "2") {
+        console.log("Paper!");
+        return weapon;
+    } else if  (weapon === "scissor" || weapon === "Scissor" || weapon === "3" ) {
+        console.log("Scissor!");
+        return weapon;
+    } else {
+        
+        // If user did not choose from selection
+        // function calls itself
+        console.log("You did not choose from the selection.");
+       user_Input(); 
+    }
+    };
+
+var computer_Choice = function () {
+    var comp_weapon = Math.floor(Math.random() * (4 - 1) + 1);
+    return selection_List(comp_weapon);
+};
+
+var game = function () {
+    
+    intro();
+    var human = user_Input();
+    console.log(human);
+    };
+    
+game();
+
