c914f15054a524172638385cedd1b9b23a63a855
Anthony Albertorio <anthony.albertorio@gmail.com>
2014-12-12 13:57:54 -0500

    added array and document.write to finding_multiples...js program

--- finding_multiples_with_variable_factors.js
+++ finding_multiples_with_variable_factors.js
@@ -2,20 +2,25 @@
 // finding all the multiples of a range of number
 // aka find all those divisible by a number selected by user.
 
-
-
 console.log("Enter the max number:");
 var range = prompt("Enter the max number:");
 
 console.log("Enter a factor:");
 var div = prompt("Enter a factor:");
 
-for (i = 1; i < range; i++){
+var multiples = [];
+
+for (i = 1; i <= range; i++){
     if (i % div === 0){
     	// console output for testing on repl.it
         console.log(i);
 
-        // for output on html
-        document.write(i + ", ");
+        //push into array
+        multiples.push(i);
+       
     }
     }
+
+document.write("\n");
+
+document.write(multiples.join(", "));
