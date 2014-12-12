f6731f85326e8b3d21cd7e35a8dca89ba37ac071
Anthony Albertorio <anthony.albertorio@gmail.com>
2014-12-12 13:41:35 -0500

    added abilty to output finding_multiples...js on html page

--- finding_multiples_with_variable_factors.js
+++ finding_multiples_with_variable_factors.js
@@ -2,14 +2,20 @@
 // finding all the multiples of a range of number
 // aka find all those divisible by a number selected by user.
 
+
+
 console.log("Enter the max number:");
-var range = prompt("Enter the max number");
+var range = prompt("Enter the max number:");
 
 console.log("Enter a factor:");
-var div = prompt("Enter a number");
+var div = prompt("Enter a factor:");
 
-for (i = 1; i < 11; i++){
+for (i = 1; i < range; i++){
     if (i % div === 0){
+    	// console output for testing on repl.it
         console.log(i);
+
+        // for output on html
+        document.write(i + ", ");
     }
     }
