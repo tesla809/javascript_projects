3ac1773bee492d1f70264a909dcb8cc591c73663
Anthony Albertorio <anthony.albertorio@gmail.com>
2014-12-12 17:19:12 -0500

    added rules explained option to digital_celo.js

--- digital_celo.js
+++ digital_celo.js
@@ -15,29 +15,43 @@
 //prompts:
 // comp or number of people playing?
 
-
-
 var intro = function (){
-    console.log ("Welcome to Celo Dice! \n");
-    
-    //rules
-    console.log ("The Rules:\n There are three dice.");
-    console.log (" Each player rolls once.");
-    console.log (" Must roll untill pattern is found.");
-    
-    console.log("\n");
-    
-    //patterns
-    console.log ("The Patterns: \n 4-5-6 highest pattern");
-    console.log (" Trips- triples of dice. higher triples wins");
-    console.log (" Point- doubles with number. Higher single wins");
-    console.log (" 1-2-3 - loses automatically");
+    console.log ("Welcome to Celo Dice!\n");
 };
 
+var players = function (){
+    
+};
+
+var rules_Explain = function (){
+    
+    console.log("See rules? Yes or No");
+    var see_Rules = prompt ("See rules? Yes or No");
+    see_Rules =  see_Rules.toUpperCase();
+    
+    //rules
+    if (see_Rules === "YES" || see_Rules === "Y"){
+        console.log ("\n The Rules:\n There are three dice.");
+        console.log (" Each player rolls once.");
+        console.log (" Must roll untill pattern is found.\n");
+        console.log ("The Patterns: \n 4-5-6 highest pattern");
+    console.log (" Trips- triples of dice. higher triples wins");
+    console.log (" Point- doubles with number. Higher single wins");
+    console.log (" 1-2-3 - loses automatically"); 
+     
+    } else if (see_Rules === "NO" || see_Rules === "N") {
+        // enter game
+        console.log("enter game place holder");
+    } else {
+        console.log("Pick a valid entry yo");
+        rules_Explain();
+    }
+};
 
 
 var game = function()  {
     intro();
+    rules_Explain();
 };
 
 // start game.
