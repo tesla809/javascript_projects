cd2afa19c7daa17580d7fca7c92d6cd92bc9dccf
Anthony Albertorio <anthony.albertorio@gmail.com>
2014-12-30 16:17:25 -0500

    updated FirstFactorial.js with better base case definitions

--- factorial_function.js
+++ factorial_function.js
@@ -3,7 +3,10 @@ num = prompt("factorial is: ");
 FirstFactorial(num);
 
 function FirstFactorial(num) {
-    if(num === 0){
+    if (num < 0){
+        return 0;
+    }
+    else if (num === 0){
         return 1;
     } else {
         //the recursion....
