0b1691c3ecafd7cb75952581edc88b17e13fee77
Anthony Albertorio <anthony.albertorio@gmail.com>
2015-01-08 14:48:19 -0500

    updated anyRandomNum.js to check for integers in order to run

--- anyRandomNum.js
+++ anyRandomNum.js
@@ -1,9 +1,23 @@
 
 //function for generating any random number
 function getRandomNumber( lower, upper ){
-  var randomNumber = Math.floor( Math.random() * (upper - lower) + 1) + lower;
-  randomNumber = parseInt(randomNumber);
-  return randomNumber;
+    // turns strings into integers
+    lower = parseInt(lower);
+    upper = parseInt(upper);
+    
+    //checks if arguments are numbers
+    if (isNaN(lower) === false && isNaN(upper) === false){
+        var randomNumber = Math.floor( Math.random() * (upper - lower) + 1) + lower;
+    return randomNumber;
+    } else {
+        console.log("Please pick two integers. One for the lower and another for the upper range");
+    }
 }
 
-console.log( getRandomNumber( 22, 24) );
+console.log("Pick the lower range" );
+var low = prompt("Pick the lower range");
+
+console.log("Pick the upper range" );
+var high = prompt("Pick the upper range");
+
+console.log( getRandomNumber( low, high) );
