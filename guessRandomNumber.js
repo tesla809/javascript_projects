c1af93ec3892db41096c0c4266616ba83798396f
Anthony Albertorio <anthony.albertorio@gmail.com>
2015-02-08 18:12:03 -0500

    refactored code to have everything in one function

--- guessRandomNumber.js
+++ guessRandomNumber.js
@@ -1,26 +1,31 @@
-function guessNum() {
-    function randomNumber(upper){
-        var randomNum = Math.floor(Math.random() * upper) + 1;
-        return randomNum;
-    }
-    
-    // set random numbers
-    var randNum = randomNumber(upper);
-    var guessNumber = randomNumber(upper);
-    var counter = 0;
 
-    // guess loop
-    while(randNum != guessNumber){
-        console.log("number: " + guessNumber + " is not it");
-        counter += 1;
-        
-        // re-guess
-        guessNumber = randomNumber(upper);
-        }
-        
-    console.log("It took " + counter + " tries to guess the random number: " + randNum);
-}
+function play() {
 console.log("Enter upper limit of guessing");
 var upper = parseInt(prompt());
     
+    function guessNum() {
+        function randomNumber(upper){
+            var randomNum = Math.floor(Math.random() * upper) + 1;
+            return randomNum;
+        }
+        
+        // set random numbers
+        var randNum = randomNumber(upper);
+        var guessNumber = randomNumber(upper);
+        var counter = 0;
+    
+        // guess loop
+        while(randNum !== guessNumber){
+            console.log("number: " + guessNumber + " is not it");
+            counter += 1;
+            
+            // re-guess
+            guessNumber = randomNumber(upper);
+            }
+            
+        console.log("It took " + counter + " tries to guess the random number: " + randNum);
+    }
 guessNum(upper);
+}
+
+play();
